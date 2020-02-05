80249801a7830d82f0078ec4cdb7a795a7dd24c4
Stephen Dini <stephendini@gmail.com>
2020-02-04 19:14:02 -0500

    Code created to solve Question 12

---
+++ Project Euler Questions/Question 12.py
@@ -0,0 +1,32 @@
+import math
+
+
+def triangleNumber(value):
+    return (value * (value + 1)) / 2
+
+
+def FindDiv(overDiv):
+    divisors = 0
+    inc = 0
+    new = triangleNumber(overDiv)
+
+    while(True):
+        divisors = 0
+        sqrt = int(math.sqrt(new))
+
+        for x in range(1, int(math.sqrt(new))):
+            if((new % x) == 0):
+                divisors += 2
+        if(sqrt * sqrt == new):
+            divisors -= 1
+
+        if(divisors > overDiv):
+            return new
+        else:
+            inc += 1
+            new = int(triangleNumber(overDiv + inc))
+
+
+answerNew = FindDiv(500)
+
+print(answerNew, " is the answer")
