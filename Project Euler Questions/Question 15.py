b7d68cb5088144a376ee0a4532caaef5aeb6f58a
Stephen Dini <stephendini@gmail.com>
2020-02-06 18:23:24 -0500

    attempt at problem 15

--- Project Euler Questions/Question 15.py
+++ Project Euler Questions/Question 15.py
@@ -1,9 +1,26 @@
 # This will provide the answer to question 15
-mazeSmall = [[],[]]
+import pprint
 
-# set mazeSmall to 2 x 2
-for i in range(0, 1):
-    for x in range(0, 1):
-        mazeSmall[i][x].append(".")
 
-print(mazeSmall)
+mazeSmall = [[0] * 3 for i in range(0, 3)]
+pprint.pprint(mazeSmall)
+mazeLarge = [[0] * 21 for i in range(0, 21)]
+
+# if(str(input("Would you like to enter a custom square?")).lower == "y"):
+#     customValue = input("Please enter a whole number.")
+# mazeCustom = [[0] * (customValue + 1) for i in range(0, customValue + 1)]
+
+
+# only allowed to move down and right
+def findAllPaths(maze):
+    size = len(maze) - 1
+    path = []
+    allPaths = []
+    print(len(path), "asdasd")
+    # while(true):
+
+
+    # return size
+
+print(mazeLarge[20][20])
+pprint.pprint(findAllPaths(mazeLarge))
