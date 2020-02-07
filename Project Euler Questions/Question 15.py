2dda1166ae9c6fedb392ad3c1df9c93cc0e3cbe6
Stephen Dini <stephendini@gmail.com>
2020-02-06 19:30:36 -0500

    Found a quicker solution using (2m)! / (m!) * 2 where m is the size of the of x * x grid.

--- Project Euler Questions/Question 15.py
+++ Project Euler Questions/Question 15.py
@@ -1,26 +1,6 @@
 # This will provide the answer to question 15
-import pprint
+import math
 
-
-mazeSmall = [[0] * 3 for i in range(0, 3)]
-pprint.pprint(mazeSmall)
-mazeLarge = [[0] * 21 for i in range(0, 21)]
-
-# if(str(input("Would you like to enter a custom square?")).lower == "y"):
-#     customValue = input("Please enter a whole number.")
-# mazeCustom = [[0] * (customValue + 1) for i in range(0, customValue + 1)]
-
-
-# only allowed to move down and right
-def findAllPaths(maze):
-    size = len(maze) - 1
-    path = []
-    allPaths = []
-    print(len(path), "asdasd")
-    # while(true):
-
-
-    # return size
-
-print(mazeLarge[20][20])
-pprint.pprint(findAllPaths(mazeLarge))
+# because there are only 2 choices we can find it the following way. 
+n = 20
+print((math.factorial((n * 2)) / (math.factorial(n)**2)))
