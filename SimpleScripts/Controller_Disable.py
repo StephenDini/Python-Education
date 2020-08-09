2753664a8d7924c2a926de75e440395a9e12e7ae
Stephen Dini <stephendini@gmail.com>
2020-08-09 11:56:13 -0400

    Finished Battleship example.

--- SimpleScripts/Controller_Disable.py
+++ SimpleScripts/Controller_Disable.py
@@ -1,27 +1,122 @@
+from PyQt5 import QtWidgets, QtCore, uic, QtGui
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import QDialogButtonBox
 import subprocess
 import pprint
+import os, sys
 pp = pprint.PrettyPrinter()
 
-# find all devices command
-Find_command = 'devcon.exe find *'
+QMainWindow = QtWidgets.QMainWindow
 
-#list HW ids of devices
-hwIds_command = 'devcon.exe hwids *'
+class MainWindow(QMainWindow):
 
-#Enable
-Enable_command = 'devcon.exe enable *PID_1016'
+    def __init__(self):
+        QMainWindow.__init__(self)
 
-#Disable
-Disable_command = 'devcon.exe disable *Razer*'
+        # self.ui = uic.loadUi("ui\keyboard.ui", self)
 
-#Find Device
-Find_SpecificDevice_command = 'devcon.exe find *Razer*'
+        self.setWindowFlags(
+            QtCore.Qt.WindowStaysOnTopHint |
+            QtCore.Qt.FramelessWindowHint |
+            QtCore.Qt.X11BypassWindowManagerHint
+        )
 
-find= True
-try:
-    if find:
-        result = subprocess.check_output(Find_SpecificDevice_command,shell=True ,stderr=subprocess.STDOUT)
+        self.setGeometry(
+            QtWidgets.QStyle.alignedRect(
+                QtCore.Qt.LeftToRight, QtCore.Qt.AlignCenter,
+                QtCore.QSize(750, 500),
+                QtWidgets.qApp.desktop().availableGeometry()
+            ))
 
-        print(result)
-except subprocess.CalledProcessError as e:
-    raise RuntimeError("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
+        self.setStyleSheet("""QToolTip { 
+                                   background-color: black; 
+                                   color: white; 
+                                   border: black solid 1px
+                                   }""")
+
+        labelNew = QtWidgets.QPushButton("test", self)
+        labelNew.setToolTip("The Key(s) are:  Modifier: ")
+        labelNew.resize(150, 30)
+        labelNew.clicked.connect(self.toggle_controller)
+        
+        self.show()
+
+    # @pyqtSlot()
+    def testClick(self):
+        print("click")
+        
+        dlg = CustomDialog(self)
+        
+        if dlg.exec_():
+            print("Success!")
+        else:
+            print("Cancel!")
+
+    def toggle_controller(self):
+        print("click")
+        
+        dlg = CustomDialog(self)
+        
+        if dlg.exec_():
+            print("Success!")
+        else:
+            print("Cancel!")
+        
+        # find all devices command
+        Find_command = 'devcon.exe find *'
+
+        #list HW ids of devices
+        hwIds_command = 'devcon.exe hwids *'
+
+        #Enable
+        Enable_command = 'devcon.exe enable *PID_1016'
+
+        #Disable
+        Disable_command = 'devcon.exe disable *Razer*'
+
+        #Find Device
+        Find_SpecificDevice_command = 'devcon.exe find *Razer*'
+
+        find= True
+
+        try:
+            if find:
+                result = subprocess.check_output(hwIds_command,shell=True ,stderr=subprocess.STDOUT)
+                print(result)
+        except subprocess.CalledProcessError as e:
+            # raise RuntimeError("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
+            output = e.output
+            results = "reboot" in str(output)
+            if results:
+                print("Would you like to reboot?")
+
+class CustomDialog(QtWidgets.QDialog):
+
+    def __init__(self, *args, **kwargs):
+
+
+        super(CustomDialog, self).__init__(*args, **kwargs)
+
+        self.setWindowTitle("Test")
+
+        QBtn = QDialogButtonBox.Ok | QDialogButtonBox.Cancel
+
+        self.buttonBox = QDialogButtonBox(QBtn)
+        self.buttonBox.accepted.connect(self.accept)
+        self.buttonBox.rejected.connect(self.reject)
+
+        self.layout = QtWidgets.QVBoxLayout()
+        self.layout.addWidget(self.buttonBox)
+        self.setLayout(self.layout)
+
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
+    
+    window.show()
+    app.exec_()
+
+if __name__ == '__main__':
+    main()
--- SimpleScripts/battleship.py
+++ SimpleScripts/battleship.py
@@ -1,7 +1,8 @@
 import random as R
 import pprint as pp
+from os import system, name
 
-
+# Fog of war revealed
 BOARD = ["-","-","-","-","-","-","-","-","-","-", #  0- 9
          "-","-","-","-","-","-","-","-","-","-", # 10-19
          "-","-","-","-","-","-","-","-","-","-", # 20-29
@@ -13,15 +14,34 @@ BOARD = ["-","-","-","-","-","-","-","-","-","-", #  0- 9
          "-","-","-","-","-","-","-","-","-","-", # 80-89
          "-","-","-","-","-","-","-","-","-","-"] # 90-99
 
+
+# What the player will see
+OBSCURD_BOARD = ["X","X","X","X","X","X","X","X","X","X", #  0X 9
+                 "X","X","X","X","X","X","X","X","X","X", # 10X19
+                 "X","X","X","X","X","X","X","X","X","X", # 20X29
+                 "X","X","X","X","X","X","X","X","X","X", # 30X39
+                 "X","X","X","X","X","X","X","X","X","X", # 40X49
+                 "X","X","X","X","X","X","X","X","X","X", # 50X59
+                 "X","X","X","X","X","X","X","X","X","X", # 60X69
+                 "X","X","X","X","X","X","X","X","X","X", # 70X79
+                 "X","X","X","X","X","X","X","X","X","X", # 80X89
+                 "X","X","X","X","X","X","X","X","X","X"] # 90-99
+
+# Ship size enums
 TINY_SHIP = 'TT'
 SMALL_SHIP = 'SSS'
 MEDIUM_SHIP = "MMMM"
 LARGE_SHIP = "LLLLL"
-DEBUG = True
+DEBUG = False
 
 gamemode = [TINY_SHIP, MEDIUM_SHIP, MEDIUM_SHIP, LARGE_SHIP, LARGE_SHIP]
 
 def place_ship():
+    """
+    Finds a set of coordinates and calls check_location() to see if its a valid spot.
+    If the location is valid place it, otherwise find a new set of coordinates.  
+    """
+    global DEBUG
     for ship in gamemode:
         
         if R.randint(0,1) == 1: # verticle
@@ -38,10 +58,11 @@ def place_ship():
                 elif ship == LARGE_SHIP:
                     first_location = R.randint(0,59)
                     location = (first_location, first_location + 10, first_location + 20, first_location + 30, first_location + 40)
-            
-                print("=======================")
-                print("Ship possible location" +  str(location))
-                print("=======================")
+
+                if DEBUG is True:
+                    print("=======================")
+                    print("Ship possible location" +  str(location))
+                    print("=======================")
 
                 results = check_location(ship, location)
 
@@ -53,6 +74,11 @@ def place_ship():
                         break
                     else:
                         print("Unsafe Spots Found!")
+                else:
+                    if results is True:
+                        for safe_spot in location:
+                            BOARD[safe_spot] = ship[-1]
+                        break
         else: #horrizontal 
             while True:
                 if ship == TINY_SHIP:
@@ -76,12 +102,7 @@ def place_ship():
                 elif ship == LARGE_SHIP:
                     while True:
                         first_location = R.randint(0,94)
-                        # print(first_location)
                         if (first_location + 4) % 10 != 0 and (first_location + 3) % 10 != 0 and (first_location + 2) % 10 != 0 and (first_location + 1) % 10 != 0:
-                            # print("==========")
-                            # print(first_location + 1)
-                            # print("==========")
-
                             break
                     location = (first_location, first_location + 1, first_location + 2, first_location + 3, first_location + 4)
             
@@ -95,11 +116,20 @@ def place_ship():
                         break
                     else:
                         print("Unsafe Spots Found!")
+                else:
+                    if results is True:
+                        for safe_spot in location:
+                            BOARD[safe_spot] = ship[-1]
+                        break
 
 
-# returns True if no unsafe locations are found, False if a ship is already in the spot
 def check_location(type,location): 
-    
+    """
+    type =  TINY_SHIP, SMALL_SHIP, MEDIUM_SHIP, LARGE_SHIP
+    location = list
+
+    Returns True if it find a suitible place for a ship and False if a ship piece is in the way.
+    """
     if type == TINY_SHIP:
         loc1,loc2 = location
         loc3 = loc4 = loc5 = "empty"
@@ -118,23 +148,23 @@ def check_location(type,location):
     for loc in BOARD:
         if counter == loc1:
             if loc != "-":
-                print(loc)
+                # print(loc)
                 return False
         if counter == loc2:
             if loc != "-":
-                print(loc)
+                # print(loc)
                 return False
         if counter == loc3:
             if loc != "-":
-                print(loc)
+                # print(loc)
                 return False
         if counter == loc4:
             if loc != "-":
-                print(loc)
+                # print(loc)
                 return False
         if counter == loc5:
             if loc != "-":
-                print(loc)
+                # print(loc)
                 return False
         
         if counter  == 99:
@@ -142,20 +172,73 @@ def check_location(type,location):
         
         counter+= 1
 
-# Prints the board in a 10 by 10
 def print_board():
-    for i, elem in enumerate(BOARD):
-        if i % 10 == 0:
+    """
+    Prints the board in a 10 by 10
+    """
+    for i, elem in enumerate(OBSCURD_BOARD):
+        if i % 10 == 0 and i !=0:
+            print(" " + str(i-10) + " - " + str(i - 1), end='') # no end of line character
             print()
-        print(str(elem).ljust(3), end='')
+        print(str(elem).ljust(3), end='') # no end of line character
+    print(" " + str(i-10) + " - " + str(i), end='') # no end of line character
     print()
+    if DEBUG is True:
+        for i, elem in enumerate(BOARD):
+            if i % 10 == 0:
+                print(" " + str(i-10) + " - " + str(i - 1), end='') # no end of line character
+                print()
+            print(str(elem).ljust(3), end='') # no end of line character
+        print(" " + str(i-10) + " - " + str(i), end='') # no end of line character
+        print()
 
-        
+total_parts_destoryed = 0 # end game at 20
+
+def make_choice(selector):
+    """
+    Return True if all parts destroyed, otherwise check for hit or miss.
+    """
+    global total_parts_destoryed
+    global DEBUG
+
+    if BOARD[int(selector)] != '-':
+        if OBSCURD_BOARD[int(selector)] != BOARD[int(selector)]:
+            total_parts_destoryed += 1
+            print("-| hit! |-")
+            OBSCURD_BOARD[int(selector)] = BOARD[int(selector)]
+        else:
+            print("You hit this already")
+    else:
+        print("-| Miss! |-")
+    
+    if total_parts_destoryed == 20:
+        return True
+    
+def clear(): 
+    """
+    clears the screen
+    """
+    # for windows 
+    if name == 'nt': 
+        _ = system('cls') 
+  
+    # for mac and linux(here, os.name is 'posix') 
+    else: 
+        _ = system('clear') 
+
 # test_location = 44,45
 
     
 # print(results)
 place_ship()
-print_board()
+# print_board()
 
-
+while True:
+    print_board()
+    response = input("Please enter the location to fire your missle: ") 
+    clear()
+    if make_choice(response)  is True:
+        print("We have a Winner!")
+        print_board()
+        break
+    
